d14d90d5671f7e1a373be157e6fac5f14f2b5a6b
Diego Vitvicki <drmv09@gmail.com>
2023-09-17 22:26:29 -0300

    ex 3
    doing ex 3 java study

---
+++ PraticaHeranca3/src/Pacote1/Funcionario.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Pacote1;
+
+/**
+ *
+ * @author mto_l
+ */
+public class Funcionario {
+    protected String nome;
+    protected int cpf;
+    protected double salario;
+    
+    public Funcionario(String nome, int CPF, double salario){
+        this.nome = nome;
+        this.cpf = cpf;
+        this.salario = salario;
+}
+    
+    
+}
